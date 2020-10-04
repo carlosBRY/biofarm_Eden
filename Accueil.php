54366b3e08fca615165bc68a48f7a03370db9875
carlosBRY <72298586+carlosBRY@users.noreply.github.com>
2020-10-04 03:13:38 +0100

    Add files via upload

---
+++ Accueil.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BIOFARM EDEN</title>
+	<meta charset="utf-8">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
+    	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+   
+	<link rel="stylesheet" type="text/css" href="css/Accueil.css">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+	
+</head>
+<body>
+<?php include("C:/MAMP/htdocs/Biofarm Eden/Includes/Header.php");?>
+
+
+
+<h1>accueil</h1>
+<div class="loginbox">
+		
+		<h1 class="connect"><strong>Connection</strong></h1><hr>
+		<form>
+			<p class="user">NOM d'utilisateur ou Email</p>
+			<input type="text"  placeholder="                                                                                    @gmail.com">
+			<p class="mdp">Mot de passe</p>
+			<input type="password" name="" placeholder="">
+			<label><p><input type="checkbox" name="checkbox">Rappelez vous de moi<p></label>
+			<input type="submit" name="" value="S'identifier" class="idt">
+			<input type="submit" name="" value="S'inscrire" class="ins">
+			<p><a href="#">Mot de passe oublié?</a>
+			<a href="#">Creer un compte</a></p>
+
+	</div>
+
+<div class="row">
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12 ">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     </div>
+     <div class="row">
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12 ">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     </div>
+     <div class="row">
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12 ">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     	<article class="col-xs-12 col-lg-4 col-md-4 col-sm-12">
+     	<h1>Mon article</h1>
+     	<p>Le risque d’enlèvement, très élevé, est croissant à mesure que l’on se rapproche des zones frontalières. Les déplacements impératifs dans les zones déconseillées (zone orange) doivent être limités dans le temps. En cas de nuitée sur place, il convient de résider dans un hôtel ou un lieu sécurisé</p>
+     	</article>
+     </div>
+<?php include("C:/MAMP/htdocs/Biofarm Eden/Includes/Footer .php");?>
+</body>
+</html>
